12122b457cbbac6d5a8cf6616a550e1b5acc3e16
abhinav <abhinavkumarmishra23@gmail.com>
2022-05-07 22:00:44 +0530

    Added Pymongo Concepts

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from sanic import Sanic, response
 import redis
 import json
+from bson import ObjectId
+
 import requests
 from datetime import timedelta
-##### INITIAL SETUP #####
+from pymongo import MongoClient
+from pprint import pprint
+
+
+##### SANIC INITIAL SETUP #####
 
 app = Sanic('school')
 
@@ -17,7 +23,12 @@ in_memory_student_db = [
     }
 ]
 
+##### REDIS INITIAL SETUP #####
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+##### MONGO INITIAL SETUP #####
+db_client = MongoClient('mongodb://localhost:27017/')
+db  = db_client.school
     
 ##### CRUD METHODS #####
 
@@ -25,33 +36,39 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 async def get_student(request) :
     id = request.args.get("id")
     if id :
-        return response.json(in_memory_student_db[int(id)])
-    return response.json(in_memory_student_db)
+        student = db.student.find_one({"_id":ObjectId(id)})
+        pprint(id)
+        return response.json(student, default=str, dumps=json.dumps)
+    students = db.student.find()
+    res = []
+    for student in students : 
+        res.append(student)
+    return response.json(res, default=str)
 
 
+# @app.post('/')
+# async def post_student(request) :
+#     student = request.json
+#     created_student = db.student.insert_one(student)
+    
+#     return response.json(created_student, default=str)
 
-@app.post('/')
-async def post_student(request) :
-    student = request.json
-    in_memory_student_db.append(student)
-    return response.json(student)
+# @app.put('/<id:int>')
+# async def update_student(request, id) :
+#     student = request.json
+#     if id in range(len(in_memory_student_db)):
+#         in_memory_student_db[id] = student
+#     else:
+#         return response.json({"error":"No Student with given id"})
+#     return response.json(student)
 
-@app.put('/<id:int>')
-async def update_student(request, id) :
-    student = request.json
-    if id in range(len(in_memory_student_db)):
-        in_memory_student_db[id] = student
-    else:
-        return response.json({"error":"No Student with given id"})
-    return response.json(student)
-
-@app.delete('/<id:int>')
-async def delete_student(request, id) :
-    if id in range(len(in_memory_student_db)):
-        del in_memory_student_db[id]
-    else:
-        return response.json({"error": "No Student with given id"})
-    return response.json({"message": "Deleted student successfully"})
+# @app.delete('/<id:int>')
+# async def delete_student(request, id) :
+#     if id in range(len(in_memory_student_db)):
+#         del in_memory_student_db[id]
+#     else:
+#         return response.json({"error": "No Student with given id"})
+#     return response.json({"message": "Deleted student successfully"})
 
 ##### REDIS CACHING OF AN API CALL #####
 @app.get('/redis')
@@ -77,4 +94,4 @@ async def get_redis_cache(request) :
 
 ##### RUN SERVER #####
 
-app.run(host='localhost', port=8000, debug=True,)
+app.run(host='localhost', port=8000, debug=True, auto_reload=True)
